f9f742c02c0f87815dd44a095fb35dab12919f00
Michael van der Werve <mvdwerve@outlook.com>
2018-10-11 13:56:41 +0200

    mailerq 5.3 was released last week

---
+++ 5.3/standalone/entry.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#   entry.sh
+#
+#   script will start up the rabbitmq server and add a user which can be
+#   externally accessed.
+#
+#   @author Michael van der Werve <michael.vanderwerve@copernica.com>
+#   @copyright 2018 Copernica BV
+#
+
+# first we are going to try to fetch the licnse; if this fails, we're lost anyway
+if [[ -n "${LICENSE_KEY}" ]]; then
+    echo "y" | mailerq --fetch-license ${LICENSE_KEY}
+elif [[ ! -f /etc/mailerq/license.txt ]]; then
+    echo "License file does not exist. Either pass a LICENSE_KEY variable using -e or bind /etc/mailerq/license.txt using -v."
+    exit 1
+fi
+
+# start the rabbitmq server and add the 'mailerq' user for external connection
+service rabbitmq-server start
+
+# check if a password for the mailerq user has been provided
+if [[ -z "${RABBITMQ_PASSWORD}" ]]; then
+    # randomly generate a password for the mailerq user
+    pass=`openssl rand -base64 32`
+    echo 'Generated random password for RabbitMQ user `mailerq`:' $pass
+    rabbitmqctl add_user mailerq $pass
+else
+    # pass was defined by docker
+    rabbitmqctl add_user mailerq $RABBITMQ_PASSWORD
+fi
+
+# process the username and password in the storage
+rabbitmqctl set_permissions -p / mailerq ".*" ".*" ".*"
+rabbitmqctl set_user_tags mailerq administrator
+
+# startup the mailerq 
+echo "Starting up MailerQ"
+mailerq
